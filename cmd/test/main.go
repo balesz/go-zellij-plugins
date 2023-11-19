dab0b7ad2cf64ee685e67ebb74b994771f457252
Balesz <balint.bode@gmail.com>
2023-11-19 17:33:03 +0100

    Update zelligo dep

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -11,20 +11,22 @@ type MyPlugin struct {
 	message string
 }
 
-func (p *MyPlugin) Load(configuration map[string]string) {
+func (p *MyPlugin) Load(configuration map[string]string) error {
 	fmt.Fprintf(os.Stderr, "configuration for plugin is: %v", configuration)
 	zelligo.Subscribe([]zelligo.EventType{
 		zelligo.EventType_CopyToClipboard,
 	})
+	return nil
 }
 
-func (p *MyPlugin) Update(event zelligo.Event) bool {
+func (p *MyPlugin) Update(event zelligo.Event) (bool, error) {
 	fmt.Fprintf(os.Stderr, "log message: %v", event)
-	return true
+	return true, nil
 }
 
-func (p *MyPlugin) Render(cols uint32, rows uint32) {
+func (p *MyPlugin) Render(cols uint32, rows uint32) error {
 	fmt.Println(p.message)
+	return nil
 }
 
 func init() {
